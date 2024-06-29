375c29e5b19e74d5595f75b703cc389de28a6f01
diogo <dsprata@gmail.com>
2024-06-29 17:50:39 -0300

    Novo commmit

--- LAB-BANCO_DIGITAL-OO/src/ContaCorrente.java
+++ LAB-BANCO_DIGITAL-OO/src/ContaCorrente.java
@@ -10,5 +10,30 @@ public class ContaCorrente extends Conta {
 		System.out.println("=== Extrato Conta Corrente ===");
 		super.imprimirInfosComuns();
 	}
+
+	@Override
+	public void pedirImprestimo(double valor) {
+		// TODO Auto-generated method stub
+		if (cliente.getIdade() < 18){
+			System.out.println("Menor de idade não é permitido pedir imprestimo");	
+		} else {
+			super.depositar(valor);
+			this.setEmprestimo(valor);
+		}
+		
+	}
+
+	@Override
+	public void sacar(double valor) {
+		if (this.saldo - valor < 0) {
+			if (Math.abs(this.saldo - valor) > cliente.limite ) {
+				System.out.println("Saque maior que o limite");	
+			} else {
+				super.sacar(valor);					
+			}	
+		} else {
+			super.sacar(valor);		
+		}	
+	}
 	
 }
